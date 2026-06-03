17002bcb83601e1982e0452e65669ff9dde86921
ummehabibasiddiquie <ummehabiba.siddiquie@transformsolution.net>
2026-06-03 10:38:23 +0530

    changes

--- billable_report_autosend.py
+++ billable_report_autosend.py
@@ -20,8 +20,8 @@ load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
 RECIPIENTS = [
     "ummehabiba.siddiquie@transformsolution.net",
     # "mohsin.pathan@transformsolution.net",
-    # "dharmesh.jotania@transformsolution.net",
-    # "venkateshwaran.iyer@transformsolution.net",
+    "dharmesh.jotania@transformsolution.net",
+    "venkateshwaran.iyer@transformsolution.net",
     # "yahya.irani@transformsolution.net",
     # "amit.mandviwala@transformsolution.net",
     # "sriman.narayan@transformsolution.net",
