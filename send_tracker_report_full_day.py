60b7e737dcc4c9348e2eda20c60e2f2d87fec6e3
ummehabibasiddiquie <ummehabiba.siddiquie@transformsolution.net>
2026-06-19 11:24:46 +0530

    Producion report testing

--- send_tracker_report_full_day.py
+++ send_tracker_report_full_day.py
@@ -15,21 +15,21 @@ load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
 
 RECIPIENTS = [
     "ummehabiba.siddiquie@transformsolution.net",
-    "mohsin.pathan@transformsolution.net",
-    "dharmesh.jotania@transformsolution.net",
-    "venkateshwaran.iyer@transformsolution.net",
-    "yahya.irani@transformsolution.net",
-    "amit.mandviwala@transformsolution.net",
-    "sriman.narayan@transformsolution.net",
-    "shirin.gafoor@transformsolution.net",
-    "avinash.dwivedi@transformsolution.net",
-    "manas.pradhan@transformsolution.net",
-    "ishan.sharma@transformsolution.net"
+    # "mohsin.pathan@transformsolution.net",
+    # "dharmesh.jotania@transformsolution.net",
+    # "venkateshwaran.iyer@transformsolution.net",
+    # "yahya.irani@transformsolution.net",
+    # "amit.mandviwala@transformsolution.net",
+    # "sriman.narayan@transformsolution.net",
+    # "shirin.gafoor@transformsolution.net",
+    # "avinash.dwivedi@transformsolution.net",
+    # "manas.pradhan@transformsolution.net",
+    # "ishan.sharma@transformsolution.net"
 ]
 
 CC_RECIPIENTS = [
-    "ashfaq@transformsolution.com",
-    "seema@transformsolution.com"
+    # "ashfaq@transformsolution.com",
+    # "seema@transformsolution.com"
 ]
 
 # -------------------------------
@@ -57,7 +57,7 @@ def fetch_data():
         report_date = today - timedelta(days=1)
 
         # TEST DATE
-        # report_date = datetime.strptime("2026-04-06", "%Y-%m-%d").date()
+        report_date = datetime.strptime("2026-06-17", "%Y-%m-%d").date()
         
         print(f"Fetching data for report date: {report_date}")  
 
