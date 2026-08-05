6dfea2bf2838a80af5d51a8db9494ffb851e60c8
ummehabibasiddiquie <ummehabiba.siddiquie@transformsolution.net>
2026-08-05 10:43:11 +0530

    Billable report change

--- billable_report_autosend.py
+++ billable_report_autosend.py
@@ -73,7 +73,7 @@ def fetch_data():
         report_date = today - timedelta(days=1)
 
         # TEST DATE
-        report_date = datetime.strptime("2026-07-31", "%Y-%m-%d").date()
+        # report_date = datetime.strptime("2026-07-31", "%Y-%m-%d").date()
         
         report_month = report_date.strftime("%b%Y").upper()
 
